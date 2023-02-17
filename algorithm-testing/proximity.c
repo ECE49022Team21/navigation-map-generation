350dcac83985c318e4f48f57a34347f12b0a3f40
Justin Lee <just007in2000@gmail.com>
2023-02-17 11:45:49 -0500

    updated distance calc to use pythagorean theorem due to projection computation

--- algorithm-testing/proximity.c
+++ algorithm-testing/proximity.c
@@ -17,28 +17,15 @@ static int right_child(int heap_index) {
 }
 
 // Euclidean Geodesic Approximation with small distance assumption
+// OR
+// Projection to cartesian coordinate system and use pythogorean theorem
 float32_t calc_squared_distance(float32_t x1, float32_t y1, float32_t x2, float32_t y2) {
-    float32_t dy = 12430.0 * (y1-y2) / 180.0;
-    printf("%f\n", dy);
-    float32_t dx = 24901.0 * (x1-x2) / 360.0 * DELTA_X_MULT;
-    printf("%f\n", dx);
-    return dy*dy + dx*dx;
+    float32_t x_2 = (x1-x2)*(x1-x2);
+    float32_t y_2 = (y1-y2)*(y1-y2);
+    return x_2 + y_2;
 }
 
 uint8_t get_nearest_node(uint8_t node) {
     return 0;
 }
-/*
-int main() {
-    float x1 = -86.9128053;
-    float y1 = 40.4249364;
-    float x2 = -86.9132483;
-    float y2 = 40.4264531;
-    // distance is 172.80 m
-    printf("%f %f\n", x1, y1);
-    printf("%f %f\n", x2, y2);
-    float dist_2 = calc_squared_distance(x1, y1, x2, y2);
-    printf("%f\n", dist_2);
-}
-*/
 
